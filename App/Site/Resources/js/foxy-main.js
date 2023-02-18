bd2830d3e316766ad8ae767438cc695b07d981cc
HappyFox.Devs <yasnoadriandatos@gmail.com>
2023-02-18 16:17:33 -0500

    Adding icon logic

---
+++ App/Site/Resources/js/foxy-main.js
@@ -0,0 +1,30 @@
+const notifyClose = document.querySelectorAll('.notify-close');
+const notifyElement = document.querySelector('.notify');
+
+function notifyFade() {
+    const fadeFrames = [
+        { opacity: 1 },
+        { opacity: 0 }
+    ];
+
+    const fadeTiming = {
+        duration: 500,
+        iterations: 1,
+    }
+
+    notifyElement.animate(fadeFrames, fadeTiming);
+
+    setTimeout(() => {
+        notifyElement.remove()
+    }, 500);
+}
+
+notifyClose.forEach(element => {
+    element.addEventListener('click', () => {
+        notifyFade()
+    })
+})
+
+setTimeout(() => {
+    notifyFade()
+}, 5000);
